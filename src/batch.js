6e20e60a8c67cb348bdbe9db116a1162468544e9
Ben Drucker <bvdrucker@gmail.com>
2014-07-23 08:38:42 -0400

    Handle requests with a mapper or reducer depending on presence of references

--- src/batch.js
+++ src/batch.js
@@ -8,15 +8,42 @@ exports.handler = function (batch, reply) {
   if (!Array.isArray(batch.payload.requests)) {
     return reply(boom.badRequest('Missing requests array'));
   }
-  Promise.map(batch.payload.requests, function (request) {
-    return batch.server.injectThen({
-      url: request.path,
-      method: request.method,
-      headers: batch.headers,
-      payload: request.payload,
-      session: batch.session
-    })
-    .get('result');
+  var responses;
+  if (internals.containsReferences(batch)) {
+    responses = Promise
+      .reduce(batch.payload.requests, function (requests, request) {
+        requests.push(request);
+        return requests;
+      }, [])
+      .reduce(function (responses, request) {
+        return internals.inject(request, batch)
+          .bind(responses)
+          .then(responses.push)
+          .return(responses);
+      }, []);
+  }
+  else {
+    responses = Promise.map(batch.payload.requests, function (request) {
+      return internals.inject(request, batch);
+    });
+  }
+  
+  responses.done(reply);
+};
+
+internals.containsReferences = function (batch) {
+  return batch.payload.requests.some(function (request) {
+    return request.references && request.references.length;
+  });
+};
+
+internals.inject = function (request, batch) {
+  return batch.server.injectThen({
+    url: request.path,
+    method: request.method,
+    headers: batch.headers,
+    payload: request.payload,
+    session: batch.session
   })
-  .done(reply);
+  .get('result');
 };
