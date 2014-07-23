2a7b3d27457f0997fff9e6d53bf9e8cf305e994f
Ben Drucker <bvdrucker@gmail.com>
2014-07-23 10:32:43 -0400

    Set up logic for parsing references inside payloads and paths

--- src/batch.js
+++ src/batch.js
@@ -12,7 +12,15 @@ exports.handler = function (batch, reply) {
   if (internals.containsReferences(batch)) {
     responses = Promise
       .reduce(batch.payload.requests, function (requests, request) {
-        requests.push(request);
+        requests.push({
+          path: internals.referencesIn(request, 'path')
+            ? internals.path(request.path)
+            : request.path,
+          method: request.method,
+          payload: internals.referencesIn(request, 'payload')
+            ? internals.payload(request.payload)
+            : request.payload
+        });
         return requests;
       }, [])
       .reduce(function (responses, request) {
@@ -35,6 +43,18 @@ internals.containsReferences = function (batch) {
   return batch.payload.requests.some(function (request) {
     return request.references && request.references.length;
   });
+};
+
+internals.referencesIn = function (request, object) {
+  return request.references && request.references.indexOf(object) !== -1;
+};
+
+internals.payload = function (payload) {
+  return payload;
+};
+
+internals.path = function (path) {
+  return path;
 };
 
 internals.inject = function (request, batch) {
