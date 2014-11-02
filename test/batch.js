0975cf7659a87d745cfcbd9831cf0758471ff335
Ben Drucker <bvdrucker@gmail.com>
2014-11-02 17:48:22 -0500

    Test request validation, simple batching, and parallel by default

--- test/batch.js
+++ test/batch.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var hapi   = require('hapi');
+var joi    = require('joi');
 var expect = require('chai').use(require('sinon-chai')).expect;
 var sinon  = require('sinon');
 var hoek   = require('hoek');
@@ -23,11 +24,29 @@ describe('batch-me-if-you-can', function () {
       options: options
     }, throwIf);
     server.pack.register(require('inject-then'), throwIf);
-    server.route({
-      method: 'post',
-      path: '/echo',
-      handler: echo
-    });
+    server.route([
+      {
+        method: 'post',
+        path: '/echo',
+        handler: echo
+      },
+      {
+        method: 'get',
+        path: '/delay/{delay?}',
+        handler: function (request, reply) {
+          setTimeout(function () {
+            reply(Date.now());
+          }, request.params.delay)
+        },
+        config: {
+          validate: {
+            params: {
+              delay: joi.number()
+            }
+          }
+        }
+      }
+    ]);
   }
 
   function batch (requests, options) {
@@ -80,6 +99,72 @@ describe('batch-me-if-you-can', function () {
       expect(response.result[0]).to.equal('Hello!');
     });
   });
+
+  it('rejects batch requests with no requests array', function () {
+    register();
+    return batch().then(function (response) {
+      expect(response.statusCode).to.equal(400);
+    });
+  });
+
+  it('rejects batch requests with empty requests array', function () {
+    register();
+    return batch([]).then(function (response) {
+      expect(response.statusCode).to.equal(400);
+    });
+  });
+
+  it('rejects requests with a non-array on requests', function () {
+    register();
+    return batch({}).then(function (response) {
+      expect(response.statusCode).to.equal(400);
+    });
+  });
+
+  it('rejects requests missing a path', function () {
+    register();
+    return batch([{}]).then(function (response) {
+      expect(response.statusCode).to.equal(400);
+    });
+  });
+
+  it('can batch simple requests', function () {
+    register();
+    var request = {
+      method: 'post',
+      path: '/echo',
+      payload: {
+        hello: 'world'
+      }
+    };
+    return batch([request, request])
+    .then(function (response) {
+      expect(response.result).to.deep.equal([{hello: 'world'}, {hello: 'world'}]);
+    });
+  });
+
+  it('can batch JSON requests', function () {
+    register();
+    var request = {
+      method: 'post',
+      path: '/echo',
+      payload: 'Hello!'
+    };
+    return batch([request, request])
+    .then(function (response) {
+      expect(response.statusCode).to.equal(200);
+      expect(response.result).to.deep.equal(['Hello!', 'Hello!']);
+    });
+  });
+
+  it('executes requests in parallel by default', function () {
+    register();
+    return batch([{path: '/delay/10'}, {path: '/delay/0'}])
+      .then(function (response) {
+        expect(response.result[0]).to.be.above(response.result[1]);
+      });
+  });
+
 
 
   // var server;
